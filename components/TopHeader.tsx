2e5fd649bfd62be44acac76a75b647e4cff62fda
NibrasHCDR <l.riveraa664@hotmail.com>
2022-12-02 03:08:25 -0300

    v0.1.1

--- components/TopHeader.tsx
+++ components/TopHeader.tsx
@@ -8,33 +8,39 @@ type Props = {}
 
 export default function TopHeader({}: Props) {
   return (
-    <header className="bg-slate-800 sticky flex items-start justify-between p-2 lg:pl-28 md:pl-28">
-    <div className=" lg:mx-4 pl-2">
+    <header className="bg-slate-800 sticky flex items-start p-2 lg:pl-28 md:pl-28">
+    <div className="flex flex-row items-center lg:mx-4 px-2 hover:bg-yellow-400 rounded-full">
         <SocialIcon
-        className="hover:bg-yellow-400 rounded-full"
+        className="rounded-full"
         url="https://api.whatsapp.com/message/ZW4NRMSDIHASO1?autoload=1&app_absent=0"
         fgColor="white"
         bgColor="transparent"
         label="+569 7450 4797"
         />
 
-        <a className="text-yellow-400 hidden md:inline-flex cursor-pointer hover:underline hover:text-white" href="https://api.whatsapp.com/message/ZW4NRMSDIHASO1?autoload=1&app_absent=0">
+        <a className="text-white hidden md:inline-flex cursor-pointer" href="https://api.whatsapp.com/message/ZW4NRMSDIHASO1?autoload=1&app_absent=0">
             +569 7450 4797
         </a>
 
+        </div>
+
+        <div className="flex flex-row items-center px-2 hover:bg-yellow-400 rounded-full">
+
         <SocialIcon
-        className="hover:bg-yellow-400 rounded-full ml-6"
+        className="rounded-full pl-6"
         url="https://instagram.com/a_m.abogados"
         fgColor="white"
         bgColor="transparent"
         />
 
-        <a className="text-yellow-400 hidden md:inline-flex cursor-pointer hover:underline hover:text-white" href="https://www.instagram.com/a_m.abogados/">
+        <a className="text-white hidden md:inline-flex cursor-pointer" href="https://www.instagram.com/a_m.abogados/">
             a_m.abogados
         </a>
-        </div>
 
-       <div className="flex flex-row items-center text-white cursor-pointer px-9 lg:mr-24 hover:bg-yellow-400 rounded-full hover:text-white">
+        </div>
+        
+
+       <div className="flex flex-row items-center ml-auto text-white cursor-pointer px-9 lg:mr-24 hover:bg-yellow-400 rounded-full">
        <SocialIcon
         className="rounded-full cursor-pointer"
         network="email"
